ac9c996e05b51958d9d5e5b84b7c6a1d044e3203
itapaullus <eww.paullus@gmail.com>
2019-06-10 19:09:46 +0300

    Доработан процесс загрузки, добавлены проверки даты

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import tkcalendar, xlsparser
 import sql_method as sql
 import zrk_info as log
 from tkinter import messagebox as mb
+
+
 # import tkSimpleDialog
 
 def createdb():
@@ -17,11 +19,13 @@ def createdb():
                                  'month': 'text',
                                  'amount': 'real',
                                  'phone': 'text'
-                                }
-                     )
+                                 }
+                      )
+
 
 class CalendarDialog(sd.Dialog):
     """Dialog box that displays a calendar and returns the selected date"""
+
     def body(self, master):
         self.calendar = tkcalendar.Calendar(master)
         self.calendar.pack()
@@ -29,12 +33,14 @@ class CalendarDialog(sd.Dialog):
     def apply(self):
         self.result = self.calendar.selection_get()
 
+
 def save_rate():
     rate = Tk()
     rate.title('Настройка комиссии')
     cd = CalendarDialog(rate)
     rate.geometry('480x320')
     rate.mainloop()
+
 
 def load_file():
     ask = CalendarDialog(window)
@@ -48,7 +54,8 @@ def load_file():
         # проверим уникальность
         conn = sql.DatabaseManager('Zirk.db')
         if not conn.isPrimary('Reestr', {'Reestr_date': ask.result}):
-            if not mb.askokcancel('Повторная загрузка', 'Реестр за эту дату уже заагружался.\nПерезатереть ранее загруженные данные?'):
+            if not mb.askokcancel('Повторная загрузка',
+                                  'Реестр за эту дату уже заагружался.\nПерезатереть ранее загруженные данные?'):
                 return
             log.ok_print('Запускаем загрузку за дату: {}'.format(str(ask.result)))
     # return
@@ -71,27 +78,27 @@ main_menu = Menu(window)
 # options_menu.add_command(label = 'Настройка')
 # Настройка
 options_menu = Menu(main_menu)
-options_menu.add_command(label = 'Настройка Ведомости')
-options_menu.add_command(label = 'Настройка Комиссии', command = save_rate)
-options_menu.add_command(label = 'Группы')
-options_menu.add_command(label = 'Пункты')
-options_menu.add_command(label = 'Создать БД', command = createdb)
+options_menu.add_command(label='Настройка Ведомости')
+options_menu.add_command(label='Настройка Комиссии', command=save_rate)
+options_menu.add_command(label='Группы')
+options_menu.add_command(label='Пункты')
+options_menu.add_command(label='Создать БД', command=createdb)
 # Загрузка
 load_menu = Menu(main_menu)
-load_menu.add_command(label = 'Загрузка Ведомости', command = load_file)
+load_menu.add_command(label='Загрузка Ведомости', command=load_file)
 # Таблица платежей
 paytable_menu = Menu(main_menu)
-paytable_menu.add_command(label = 'Долги на дату')
+paytable_menu.add_command(label='Долги на дату')
 # Прием платежей
 payprocess_menu = Menu(main_menu)
-payprocess_menu.add_command(label = 'Ввод Платежа')
-payprocess_menu.add_command(label = 'Отбор платежей')
+payprocess_menu.add_command(label='Ввод Платежа')
+payprocess_menu.add_command(label='Отбор платежей')
 # Отчеты
 report_menu = Menu(main_menu)
-report_menu.add_command(label = 'Оплаты')
-main_menu.add_cascade(label = 'Настройка', menu = options_menu)
-main_menu.add_cascade(label = 'Загрузка', menu = load_menu)
-main_menu.add_cascade(label = 'Прием платежей', menu = payprocess_menu)
-main_menu.add_cascade(label = 'Отчеты', menu = report_menu)
+report_menu.add_command(label='Оплаты')
+main_menu.add_cascade(label='Настройка', menu=options_menu)
+main_menu.add_cascade(label='Загрузка', menu=load_menu)
+main_menu.add_cascade(label='Прием платежей', menu=payprocess_menu)
+main_menu.add_cascade(label='Отчеты', menu=report_menu)
 window.config(menu=main_menu)
 window.mainloop()
